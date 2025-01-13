03d13c3d2297edc760338f2b384146c110871473
sawood6e <swood3@cps.edu>
2025-01-13 11:53:59 -0600

    Add files via upload

---
+++ ClickbateHeadlines.py
@@ -0,0 +1,24 @@
+#Sadie Wood
+#Clickbate Headlines
+#October 11, 2024
+
+#functions
+def headLine1():
+    num=input("Please input a number")
+    noun=input("Please input a noun")
+    place=input("Please input a place")
+    print(num + (" reasons why you need a ") + noun + (" before you go to ") + place)
+def headLine2():
+    occupation=input("Please input a occupation")
+    pronoun=input("Please input a pronoun")
+    foodItem=input("Please input a food item")
+    print(("You wont believe what this ") + occupation + (" discovered in ")  + pronoun + (" ") + foodItem)
+def headLine3():
+    num=input("Please input a number ")
+    adjective=input("Please input an adjective ")
+    awardShow=input("Please input an award show ")
+    print(num + (" ") + adjective + (" moments from the ") + awardShow + (" that you shouldn't miss"))
+#Main
+headLine1()
+headLine2()
+headLine3()
